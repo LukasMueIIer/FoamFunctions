8c953d24743b6d72f4441e28030b8d0515ce8595
Lukas Müller <lukas.mueller@ila.uni-stuttgart.de>
2024-08-14 14:48:33 +0200

    Added a function for a basic blockMesh run

--- FoamFunctions/Tools/general.py
+++ FoamFunctions/Tools/general.py
@@ -1,6 +1,7 @@
 #generally helpfull tools that fit no specific category
 import os
 from PyFoam.RunDictionary.SolutionDirectory import SolutionDirectory
+from PyFoam.Execution.BasicRunner import BasicRunner
 import shutil
 
 def get_start_path():   
@@ -18,3 +19,11 @@ def clean_parallel_and_postProcessing(dir_path):
     if os.path.exists(post_processing_dir):
         print(f"Removing postProcessing directory: {post_processing_dir}")
         shutil.rmtree(post_processing_dir)
+
+def run_Blockmesh(dir_path,silent=True): #basic BlockMesh execution with no addons
+    bm_runner = BasicRunner(argv=["blockMesh","-case",dir_path],silent=False)
+    bm_runner.start()
+    if bm_runner.runOK():
+        print(f"blockMesh ran successfully")
+    else:
+        print(f"blockMesh failed for case")
