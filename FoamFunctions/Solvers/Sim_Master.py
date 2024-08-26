37727cd75f673cac95105e49ecc8318f9d9a1cf5
Lukas Müller <lukas.mueller@ila.uni-stuttgart.de>
2024-08-26 10:05:36 +0200

    Working on the sim_master and sim classes

--- FoamFunctions/Solvers/Sim_Master.py
+++ FoamFunctions/Solvers/Sim_Master.py
@@ -1,12 +1,17 @@
 # class to make chaining and execting solvers easier
 from PyFoam.Execution.ParallelExecution import LAMMachine
+from PyFoam.RunDictionary.SolutionDirectory import SolutionDirectory
+from PyFoam.RunDictionary.ParsedParameterFile import ParsedParameterFile
+from PyFoam.Execution.BasicRunner import BasicRunner
+import time
 
 class sim_step:
-    def __init__(self,solver,time = 1, writeInterval = 1, dT = 1) -> None:
+    def __init__(self,solver,time = 1, writeInterval = 1, dT = 1, silent=True) -> None:
         self.time = time #timeframe which is simulated
         self.writeInterval = writeInterval #at what interval results are written
         self.dT = dT    #timestep size (or initial size if addaptive timestep)
         self.solver = solver #String which matches the Foam Command to execute the solver
+        self.silent = silent #If solver output is shown in console
         
     def inverval_splitting(self,n): #sets the writeInterval, so that n files are written
         self.writeInterval = self.time / n
@@ -18,7 +23,35 @@ class sim_master:
         self.lam = LAMMachine(nr = CPU_count)
         self.solverEndtimes = []    #simulation time until which solver was executed
         self.solverExecutionTimes = [] #physical duration which solver took to be executed
-        self.dir = dir_path #simulation directory
+        self.dir_path = dir_path #simulation directory
 
-    def execute(sim: sim_step): #execute a simulation defined as sim_step class
+    def execute(self,sim: sim_step): #execute a simulation defined as sim_step class
+        dire = SolutionDirectory(self.dir_path)
+        cD_file = ParsedParameterFile(dire.systemDir() + "/controlDict")
+
+        #write solver information to controlDict
+        cD_file["application"] = sim.solver
+        if(len(self.solverEndtimes == 0)):
+            cD_file["endTime"]  = str(sim.time)
+        else:
+            cD_file["endTime"]  = str(sim.time + self.solverEndtimes[-1])
+        cD_file["writeInterval"] = sim.writeInterval
+        cD_file["deltaT"] = sim.dT
+        cD_file.writeFile()
+
+        #run solver and measure time
+        start_time = time.time()
+        print("Executing " + sim.solver)
+        runner = BasicRunner(argv=[sim.solver, "-case", self.dir_path], silent= sim.silent, lam=self.lam)
+        runner.start()
+        if runner.runOK():
+            print(sim.solver + " ran successfully")
+        else:
+            print(sim.solver + " failed for case")
+        exec_time = time.time() - start_time
+        print(f"Execution time: {exec_time} seconds")
         
+        #save information
+        self.solverExecutionTimes.append(exec_time)
+
+
