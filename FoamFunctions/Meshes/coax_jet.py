d8288d35d563f791b14927b24093723a6742e718
Lukas Müller <lukas.mueller@ila.uni-stuttgart.de>
2024-10-24 15:04:55 +0200

    Somewhat finished Mesh

--- FoamFunctions/Meshes/coax_jet.py
+++ FoamFunctions/Meshes/coax_jet.py
@@ -2,7 +2,7 @@ import classy_blocks as cb
 import numpy as np
 import os
 
-def  wedge_mesh(directory,ri,ra,l,alpha,gamma,delta,x_count,buffer_count):    #create a classical based mesh 
+def  wedge_mesh(directory,ri,ra,l,alpha,gamma,delta,x_count,buffer_count,exp_inner,exp_outer):    #create a classical based mesh 
     
 
     file_path = directory
@@ -10,7 +10,9 @@ def  wedge_mesh(directory,ri,ra,l,alpha,gamma,delta,x_count,buffer_count):    #c
     #buildup
     shapes = []
 
-
+    #size_buffer = ((ri + l * np.tan(np.deg2rad(alpha))) - (ri - delta + l * np.tan(np.deg2rad(alpha - gamma))))/ (buffer_count)
+    size_buffer = delta / (buffer_count)
+    size_buffer = size_buffer * (ri - delta + l * np.tan(np.deg2rad(alpha - gamma))) / (ri - delta)
     points_inner = [[0,0,0],[l,0,0],[l,ri - delta + l * np.tan(np.deg2rad(alpha - gamma)),0],[0,ri - delta,0]]
     face_inner = cb.Face(points_inner)
     wedge_inner = cb.Wedge(face_inner)
@@ -19,7 +21,7 @@ def  wedge_mesh(directory,ri,ra,l,alpha,gamma,delta,x_count,buffer_count):    #c
     wedge_inner.set_patch("right","outlet")
 
     wedge_inner.chop(0,count = x_count)
-    wedge_inner.chop(1,count = 10)
+    wedge_inner.chop(1,end_size = size_buffer,c2c_expansion = 1/exp_inner)
 
     shapes.append(wedge_inner)
 
@@ -44,13 +46,22 @@ def  wedge_mesh(directory,ri,ra,l,alpha,gamma,delta,x_count,buffer_count):    #c
     wedge_buffer_outer.chop(1,count = buffer_count)
     shapes.append(wedge_buffer_outer)
 
+    points_outer = [[0,ri+delta,0],[l,ri + delta + l * np.tan(np.deg2rad(alpha + gamma)),0],[l,ra,0],[0,ra,0]]
+    face_outer = cb.Face(points_outer)
+    wedge_outer = cb.Wedge(face_outer)
+
+    wedge_outer.set_patch("left","inlet_outer")
+    wedge_outer.set_patch("right","outlet")
+    wedge_outer.chop(0,count = x_count)
+    wedge_outer.chop(1,start_size = size_buffer, c2c_expansion = exp_outer)
+    shapes.append(wedge_outer)
 
     # add everything to mesh
     mesh = cb.Mesh()
     for shape in shapes:
         mesh.add(shape)
 
-    #mesh.set_default_patch("far_field","patch")
+    mesh.set_default_patch("upper","patch")
 
     #debugging mode 
     #mesh.write(file_path + "/system/blockMeshDict", "debug.vtk")
