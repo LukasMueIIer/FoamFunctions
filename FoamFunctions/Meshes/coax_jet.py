09ae89d19ba5accc190b85c890aea09e372738c7
Lukas Müller <lukas.mueller@ila.uni-stuttgart.de>
2024-10-25 17:31:46 +0200

    Added the two parts, lets soo

--- FoamFunctions/Meshes/coax_jet.py
+++ FoamFunctions/Meshes/coax_jet.py
@@ -72,7 +72,7 @@ def  wedge_mesh(directory,ri,ra,l,alpha,gamma,delta,x_count,buffer_count,exp_inn
     mesh.write(file_path + "/system/blockMeshDict")
 
 
-def  wedge_mesh_piped(directory,ri,ra,l,l_pipe,alpha,gamma,delta,x_count,buffer_count,exp_inner,exp_outer):    #create a classical based mesh 
+def  wedge_mesh_piped(directory,ri,ra,l,l_pipe,alpha,gamma,delta,x_count,buffer_count,exp_inner,exp_outer,exp_pipe):    #create a classical based mesh 
     
     file_path = directory
 
@@ -110,21 +110,39 @@ def  wedge_mesh_piped(directory,ri,ra,l,l_pipe,alpha,gamma,delta,x_count,buffer_
     face_buffer_outer = cb.Face(points_buffer_outer)
     wedge_buffer_outer = cb.Wedge(face_buffer_outer,angle=ang)
 
-    wedge_buffer_outer.set_patch("left","inlet_outer")
     wedge_buffer_outer.set_patch("right","outlet")
     wedge_buffer_outer.chop(0,count = x_count)
     wedge_buffer_outer.chop(1,count = buffer_count)
     shapes.append(wedge_buffer_outer)
 
+
     points_outer = [[0,ri+delta,0],[l,ri + delta + l * np.tan(np.deg2rad(alpha + gamma)),0],[l,ra,0],[0,ra,0]]
     face_outer = cb.Face(points_outer)
     wedge_outer = cb.Wedge(face_outer,angle=ang)
 
-    wedge_outer.set_patch("left","inlet_outer")
     wedge_outer.set_patch("right","outlet")
     wedge_outer.chop(0,count = x_count)
     wedge_outer.chop(1,start_size = size_buffer, c2c_expansion = exp_outer)
     shapes.append(wedge_outer)
+
+
+    points_pipe_buffer = [[-1 * l_pipe,ri,0],[0,ri,0],[0,ri+delta,0],[-1 * l_pipe,ri+delta,0]]
+    face_pipe_buffer = cb.Face(points_pipe_buffer)
+    wedge_pipe_buffer = cb.Wedge(face_pipe_buffer)
+
+    wedge_pipe_buffer.set_patch("left","inlet_outer")
+    wedge_pipe_buffer.chop(0,end_size = l/x_count, c2c_expansion = 1 / exp_pipe)
+    wedge_pipe_buffer.chop(1,count = buffer_count)
+    shapes.append(wedge_pipe_buffer)
+
+    points_pipe_outer = [[-1 * l_pipe,ri + delta,0],[0, ri + delta,0],[0,ra,0],[-1 * l_pipe,ra,0]]
+    face_pipe_outer = cb.Face(points_pipe_outer)
+    wedge_pipe_outer = cb.Wedge(face_pipe_outer)
+
+    wedge_pipe_outer.set_patch("left","inlet_outer")
+    wedge_pipe_outer.chop(0,end_size = l/x_count, c2c_expansion = 1 / exp_pipe)
+    wedge_pipe_outer.chop(1,start_size = size_buffer, c2c_expansion = exp_outer)
+    shapes.append(wedge_pipe_outer)
 
     # add everything to mesh
     mesh = cb.Mesh()
