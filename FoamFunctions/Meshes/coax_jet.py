f650fb6c78030a75ec769e37e868a4f2b13cf9bf
Lukas Müller <lukas.mueller@ila.uni-stuttgart.de>
2025-01-16 16:35:44 +0100

    Tweaks so the patches match and dissapear

--- FoamFunctions/Meshes/coax_jet.py
+++ FoamFunctions/Meshes/coax_jet.py
@@ -246,7 +246,7 @@ def optimized_wedge_piped(directory,ri,ra,l,l_pipe,alpha,x_count,core_y_refine,e
     mesh.write(file_path + "/system/blockMeshDict")
 
 
-def piped_double_stacked_wedge_mesh(directory,ri,ra,l,l_pipe,spread_angle,x_count,y_count,buffer_count,exp_Inlet,exp_Farfield):    #create a classical based mesh 
+def piped_double_stacked_wedge_mesh(directory,ri,ra,l,l_pipe,spread_angle,x_count,y_count,exp_Inlet,exp_Farfield):    #create a classical based mesh 
     
     file_path = directory
 
@@ -254,6 +254,8 @@ def piped_double_stacked_wedge_mesh(directory,ri,ra,l,l_pipe,spread_angle,x_coun
     shapes = []
     ang = np.deg2rad(2)
 
+    x_size = l / (x_count)
+    y_size = ri / y_count
     
     points_inner = [[0,0,0],[l,0,0],[l,ri,0],[0,ri,0]]
     face_inner = cb.Face(points_inner)
@@ -262,8 +264,8 @@ def piped_double_stacked_wedge_mesh(directory,ri,ra,l,l_pipe,spread_angle,x_coun
     wedge_inner.set_patch("left","inlet_inner")
     wedge_inner.set_patch("right","outlet")
 
-    wedge_inner.chop(0,count = x_count)
-    wedge_inner.chop(1,count = y_count)
+    wedge_inner.chop(0,count=x_count)
+    wedge_inner.chop(1,count=y_count)
 
     shapes.append(wedge_inner)
 
@@ -273,7 +275,7 @@ def piped_double_stacked_wedge_mesh(directory,ri,ra,l,l_pipe,spread_angle,x_coun
     x_target = 0.25 * l_pipe
     y_target = l * np.tan(np.deg2rad(spread_angle))
 
-    x_size = l / x_count
+    x_size = l / (x_count)
     y_size = ri / y_count
 
     x_ext_count = np.rint(x_target / x_size)
@@ -282,25 +284,45 @@ def piped_double_stacked_wedge_mesh(directory,ri,ra,l,l_pipe,spread_angle,x_coun
     x_ext = -1 * x_ext_count * x_size
     y_ext = y_ext_count * y_size
 
-    points_buffer_inner = [[x_ext,ri,0],[l,ri ,0],[l,ri + y_ext ,0],[x_ext,ri + y_ext,0]]
+    points_buffer_inner = [[0,ri,0],[l,ri ,0],[l,ri + y_ext ,0],[0,ri + y_ext,0]]
     face_buffer_inner = cb.Face(points_buffer_inner)
     wedge_buffer_inner = cb.Wedge(face_buffer_inner,angle=ang)
 
     wedge_buffer_inner.set_patch("right","outlet")
-    wedge_buffer_inner.chop(0,start_size=x_size)
-    wedge_buffer_inner.chop(1,start_size=y_size)
+    wedge_buffer_inner.chop(0,count = x_count)
+    wedge_buffer_inner.chop(1,count = y_ext_count)
     shapes.append(wedge_buffer_inner)
 
+
+    #Cut it in two, for the patches
+    points_buffer_inner = [[x_ext,ri,0],[0,ri ,0],[0,ri + y_ext ,0],[x_ext,ri + y_ext,0]]
+    face_pipe_buffer = cb.Face(points_buffer_inner)
+    wedge_pipe_buffer = cb.Wedge(face_pipe_buffer)
+
+    wedge_pipe_buffer.set_patch("front","pipe")
+    wedge_pipe_buffer.chop(0,count=x_ext_count)
+    wedge_pipe_buffer.chop(1,count=y_ext_count)
+    shapes.append(wedge_pipe_buffer)
+
+
     #Circular part, that goes up towards the far field
-    points_buffer_outer = [[x_ext,ri + y_ext,0],[l,ri + y_ext,0],[l,ra,0],[x_ext,ra,0]]
+    points_buffer_outer = [[0,ri + y_ext,0],[l,ri + y_ext,0],[l,ra,0],[0,ra,0]]
     face_buffer_outer = cb.Face(points_buffer_outer)
     wedge_buffer_outer = cb.Wedge(face_buffer_outer,angle=ang)
 
     wedge_buffer_outer.set_patch("right","outlet")
-    wedge_buffer_outer.chop(0,start_size=x_size)
+    wedge_buffer_outer.chop(0,count=x_count)
     wedge_buffer_outer.chop(1,start_size=y_size,c2c_expansion=exp_Farfield)
     shapes.append(wedge_buffer_outer)
 
+
+    points_buffer_outer = [[x_ext,ri + y_ext,0],[0,ri + y_ext,0],[0,ra,0],[x_ext,ra,0]]
+    face_buffer_outer = cb.Face(points_buffer_outer)
+    wedge_buffer_outer = cb.Wedge(face_buffer_outer,angle=ang)
+
+    wedge_buffer_outer.chop(0,count=x_ext_count)
+    wedge_buffer_outer.chop(1,start_size=y_size,c2c_expansion=exp_Farfield)
+    shapes.append(wedge_buffer_outer)
 
 
     #Part at the "Inlet where the mesh gets lees defined" in only x_direction
